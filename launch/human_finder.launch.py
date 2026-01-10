d41afd092bd4950db5ce1d6d3254de6b9ed1f8ef
Mauricio Pimenta <mauriciobpimenta@gmail.com>
2026-01-09 21:14:28 -0300

    created launcher for human finder

---
+++ launch/human_finder.launch.py
@@ -0,0 +1,38 @@
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument, EmitEvent
+from launch.events.matchers import matches_action
+from launch.substitutions import LaunchConfiguration
+from launch_ros.actions import LifecycleNode
+from launch_ros.events.lifecycle import ChangeState
+from lifecycle_msgs.msg import Transition
+from ament_index_python.packages import get_package_share_directory
+import os
+
+
+def generate_launch_description():
+    use_sim_arg = DeclareLaunchArgument("use_sim", default_value="False", description="Use sim time.")
+    human_detector = LifecycleNode(
+        package="human_detector",
+        executable="human_detector",
+        name="human_detector",
+        output="screen",
+        namespace="",
+        parameters=[
+            {"use_sim_time": LaunchConfiguration("use_sim")},
+        ],
+    )
+
+    move_human_detector_to_configure_state_event = EmitEvent(
+        event=ChangeState(
+            lifecycle_node_matcher=matches_action(human_detector),
+            transition_id=Transition.TRANSITION_CONFIGURE,
+        )
+    )
+
+    return LaunchDescription(
+        [
+            use_sim_arg,
+            human_detector,
+            move_human_detector_to_configure_state_event,
+        ]
+    )
